0bb6def2cae29efe28648a29dd82d3f55d5050ad
KTomy7 <2k7r8dgv7f@privaterelay.appleid.com>
2022-07-11 16:13:35 +0300

    Some changes

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ const maxScoreInput = document.querySelector('#maxScore');
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#resetButton');
+const playButton = document.querySelector('#play');
+const formContent = document.querySelector('form');
+const cardContent = document.querySelector('.content');
+const cardFooter = document.querySelector('.card-footer');
+const scoresTable = document.querySelector('#scores');
+const table = document.querySelector('tbody');
+const p1Name = formContent.elements.p1Name;
+const p2Name = formContent.elements.p2Name;
+
 let maxScore = 7;
+let count = 1;
+
+playButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    cardContent.classList.remove('is-hidden');
+    cardFooter.classList.remove('is-hidden');
+    formContent.classList.add('is-hidden');
+    p1Button.innerText += ` ${p1Name.value}`;
+    p2Button.innerText += ` ${p2Name.value}`;
+});
 
 maxScoreInput.addEventListener('change', () => {
     maxScore = parseInt(maxScoreInput.value);
@@ -21,6 +40,7 @@ p1Button.addEventListener('click', () => {
         p2Score.classList.add('has-text-danger');
         p1Button.disabled = true;
         p2Button.disabled = true;
+        addRow(p1Name.value, score, parseInt(p2Score.innerText));
     }
     p1Score.textContent = score;
 });
@@ -35,6 +55,7 @@ p2Button.addEventListener('click', () => {
         p1Score.classList.add('has-text-danger');
         p1Button.disabled = true;
         p2Button.disabled = true;
+        addRow(p2Name.value, score, parseInt(p1Score.innerText));
     }
     p2Score.textContent = score;
 });
@@ -48,4 +69,20 @@ function reset() {
     p2Score.classList.remove('has-text-success', 'has-text-danger');
     p1Button.disabled = false;
     p2Button.disabled = false;
+}
+
+function addRow(winnerName, winnerScore, loserScore) {
+    scoresTable.classList.remove('is-hidden');
+    const newRow = document.createElement('tr');
+    const number = document.createElement('th');
+    number.append(count);
+    const winner = document.createElement('td');
+    winner.append(winnerName);
+    const score = document.createElement('td');
+    const bTag = document.createElement('b');
+    bTag.append(winnerScore);
+    score.append(bTag, ` - ${loserScore}`);
+    newRow.append(number, winner, score);
+    table.append(newRow);
+    count++;
 }
